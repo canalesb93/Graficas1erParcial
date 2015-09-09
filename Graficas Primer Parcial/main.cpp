93398cbeb42b88a779a389aff5375ce3ed1d8806
Ricardo Canales <canalesb93@gmail.com>
2015-09-08 20:44:27 -0500

    Initial Commit

---
+++ Graficas Primer Parcial/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Graficas Primer Parcial
+//
+//  Created by Ricardo Canales on 9/8/15.
+//  Copyright (c) 2015 canalesb. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
