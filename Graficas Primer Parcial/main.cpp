a52be2d0f81b90aa4ced4eaf3d312c1c6fbd56a5
MarcoRmz <marcormz19@gmail.com>
2015-09-09 13:16:58 -0500

    added pattern

--- Graficas Primer Parcial/main.cpp
+++ Graficas Primer Parcial/main.cpp
@@ -245,13 +245,13 @@ void display() {
                 else glColor3f(0.5f, 0.0f, 0.3f);
             }
             currentRectangle++;
+            glRectf(x1,y1, x2, y2);
             
             glEnable (GL_POLYGON_STIPPLE);
-            //glColor3f (1.0, 1.0, 1.0);
-            glRectf(x1,y1, x2, y2);
+            glColor3f (0.0, 0.0, 0.0);
             glPolygonStipple (sym);
-            
-
+            glRectf(x1,y1, x2, y2);
+            glDisable (GL_POLYGON_STIPPLE);
             
             x1 += cardWidth;
             x2 += cardWidth;
@@ -266,12 +266,14 @@ void display() {
                 else glColor3f(0.0f, 0.3f, 0.5f);
             }
             currentRectangle++;
+            glRectf(x1,y1,x2,y2);
             
-            //glColor3f (1.0, 1.0, 1.0);
+            glEnable (GL_POLYGON_STIPPLE);
+            glColor3f (0.0, 0.0, 0.0);
             glPolygonStipple (sym);
+            glRectf(x1,y1, x2, y2);
             glDisable (GL_POLYGON_STIPPLE);
             
-            glRectf(x1,y1,x2,y2);
             x1 += cardWidth;
             x2 += cardWidth;
         }
